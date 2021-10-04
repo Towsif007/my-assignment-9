7f60b9d488ee49519866a41492b9081650fe1e13
TowsifHasan <towsiafehasan007@gmail.com>
2021-10-04 19:23:07 +0600

    my-forth-commit

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,9 +4,21 @@ import './About.css';
 const About = () => {
     return (
         <div className="about">
-           <h1 className="text-danger">ABOUT US</h1>
-          <div className="card-body fs-3 p-3 mb-2 bg-dark text-white about-paragraph rounded-2">
+           <h1 className="text-danger fst-italic">ABOUT US</h1>
+          <div className="card-body fs-3 p-3 mb-2 bg-secondary p-2 text-dark bg-opacity-50 about-paragraph rounded-2">
           <p>We will be so much happy if you come here. We assure you that we shall give you our best services. In our courses, you can learn a lot of things. Lexicon offers the Best IELTS Preparation Course and IELTS MOCK TEST COURSE. It is the best institution for achieving the highest IELTS band score. <br/> We also have the Most Popular Courses like Practical Spoken English, Basic writing ,Phonetics, JSC Spoken, SSC Spoken and EYL Programme (Kids English and Teen English) for improving your English Language Skills in a convenient way.</p>
+          </div>
+          <div className="card-body fs-3 p-3 mb-2 bg-secondary p-2 text-dark bg-opacity-50 about-paragraph rounded-2">
+          <p>We are so much professional about this sector. We are one of the best ielts center in this country. <br/> If you come here , you will be benifitted by us.  Our so many students got 8 out of 9. It is a big factor for us.
+          some of students names are:
+          <br/>
+          1. Towsif Hasan.(8) <br/>
+          2. Shakib Hasan.(8.5) <br/>
+          3. Faisal Islam Akib.(8) <br/>
+          4. Rafiqul Hasan.(7.8) <br/>
+          5. Shafiqul Akbar.(8.5) <br/>
+          6. Townik Hasan.(7.5) <br/>
+          </p>
           </div>
         </div>
     );
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,9 +4,12 @@ import './Footer.css';
 const Footer = () => {
     return (
         
-         <div className="container-fluid bg-secondary py-5">
-             <p>Phone no. 01794710666</p>   
-            <h1>LEXICON IELTS CENTER</h1>
+         <div className="container-fluid bg-dark py-5 fw-bold text-white">
+             <h1 className="fst-italic">LEXICON IELTS CENTER</h1>
+             <h3 className="text-warning">Contact Us:</h3>
+             <p> Our Email: towsiafehasan007@gmail.com</p>
+             <p>Phone no: 01794710666</p>   
+            
         </div>
         
     );
